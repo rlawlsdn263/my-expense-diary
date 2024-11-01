76905232290ca82745d0c43b1f562e8f304902eb
김진우 <rlawlsdn263@naver.com>
2024-11-01 15:03:16 +0900

    [UI] ExpenseForm 컴포넌트를 Home 페이지에 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import styles from './page.module.css';
 
+import { ExpenseForm } from './components';
+
 export default function Home() {
-  return <main className={styles.Home}></main>;
+  return (
+    <main className={styles.Home}>
+      <ExpenseForm />
+    </main>
+  );
 }
